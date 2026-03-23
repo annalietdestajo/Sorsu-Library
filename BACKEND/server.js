fe2c8f5dfed757472ff855c5cd56c7be4d360fbf
annalietdestajo <destajoannalie01@gmail.com>
2026-03-22 21:48:13 -0700

    update

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -7,10 +7,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Library Visitor Monitoring System API is running!");
-});
-
 const path = require("path");
 app.use(express.static(path.join(__dirname, "FRONTEND")));
 app.get("/", (req, res) => {
