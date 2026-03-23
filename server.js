41e4b999b9c9cd3329c9c19f9e6a900a7b257e6f
annalietdestajo <destajoannalie01@gmail.com>
2026-03-23 00:01:18 -0700

    update

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.get("/", (req, res) => {
 app.get("/admin", (req, res) => {
   res.sendFile(path.join(__dirname, "FRONTEND", "admin-login.html"));
 });
-const DB_PATH = process.env.DATABASE_PATH || "/mnt/data/database.db"; 
-const db = new sqlite3.Database(DB_PATH, err => {
+const DB_PATH = path.join("/mnt/data", "database.db");
+const db = new sqlite3.Database(DB_PATH, (err) => {
   if (err) console.error("Database error:", err.message);
   else console.log("Connected to database:", DB_PATH);
 });
