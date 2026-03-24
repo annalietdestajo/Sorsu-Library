642e057611ca04a331ebc34f14c0de7770d1e0d0
annalietdestajo <destajoannalie01@gmail.com>
2026-03-24 10:36:13 -0700

    update

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { createClient } = require('@supabase/supabase-js');
+require('dotenv').config();
 const cors = require("cors");
 const XLSX = require("xlsx");
 const path = require("path");
